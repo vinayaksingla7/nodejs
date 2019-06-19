93798824ce5fe0112f6bd68ae7277371e6b2d721
vinayaksingla7 <vnksingla@gmail.com>
2019-06-19 15:39:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine' , 'hbs');
+
+app.use(express.static( __dirname + '/public')) ; 
+
+
+app.use((req,res,next)=>
+{
+	var now = new Date().toString(); 
+	var log = now + req.method + req.url;
+	console.log(log);
+
+	fs.appendFileSync( 'server.log' , log+ '\n' ,(err) =>
+	{
+		if(err)
+			{ console.log('Unable to append to server.log')}
+	});
+
+	next();
+
+}     );
+
+// app.use((req,res,next)=>
+
+// {
+
+// 	res.render('middle.hbs');
+	
+// }
+
+//      );
+
+
+
+
+hbs.registerHelper( 'getcurrentYear' , () =>{
+return new Date().getFullYear()
+	});
+
+hbs.registerHelper('screamIt' , (text) => 
+{
+	return text.toUpperCase();
+});
+
+app.get('/' , (req,res) =>
+{
+res.render('home.hbs' , {
+	pagetitle : 'Home Page',
+	welcome: 'welcome to my website'
+	
+
+
+})
+
+});
+
+
+app.get('/about' , (req,res) =>
+{
+res.render('about.hbs' , {
+	pagetitle : 'about Page'
+	
+
+})
+
+});
+
+
+
+app.listen(3000);
